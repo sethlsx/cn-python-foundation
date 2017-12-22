e23b0c6a32953c7a7187fc86b618845767b5ce86
sethlsx <sethl@qq.com>
2017-12-22 14:28:11 +0800

    Completed task2

--- investigate texts and calls/ZH/Task2.py
+++ investigate texts and calls/ZH/Task2.py
@@ -23,3 +23,44 @@ September 2016.".
 如果键不存在于字典内，将此键加入字典，并将它的值设为给定值。
 """
 
+# 1、定义一个统计时长的函数tongjishichang()，以通话记录列表为输入，返回字典类型各号码通话时长统计
+def tongjishichang(jilu_list):
+	shichang = {}
+	for jilu in jilu_list:
+		if jilu[0] in shichang:
+			shichang[jilu[0]] += int(jilu[3])
+		else:
+			shichang[jilu[0]] = int(jilu[3])
+
+		if jilu[1] in shichang:
+			shichang[jilu[1]] += int(jilu[3])
+		else:
+			shichang[jilu[1]] = int(jilu[3])
+
+	return shichang
+
+
+# 2、找到通话时长最长的号码
+
+mubiaohaoma = []
+mubiaoshichang = 0
+shichang = tongjishichang(calls)
+for haoma in shichang:
+	if shichang[haoma] > mubiaoshichang:
+		mubiaohaoma = []
+		mubiaohaoma.append(haoma)
+		mubiaoshichang = shichang[haoma]
+	elif shichang[haoma] == mubiaoshichang:
+		mubiaohaoma.append(haoma)
+	else:
+		pass
+
+# 考虑了2种情况，只有一个最长时长的号码和有多个最长时长的号码
+
+if len(mubiaohaoma) == 1:
+	print('{} spent the longest time, {} seconds, on the phone during September 2016.'.format(mubiaohaoma[0], mubiaoshichang))
+else:
+	print('There are {} numbers spent the longest time, {} seconds, on the phone during September 2016. They are as followed:\n'.format(len(mubiaohaoma), mubiaoshichang))
+	for i in range(len(mubiaohaoma)):
+		print(str(mubiaohaoma[i]) + '\n')
+
